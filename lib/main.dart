d06cc4e0026e7a1323ac40b2792033dfb66080ea
Reeba12 <reebasiddiqui456@gmail.com>
2022-07-25 20:41:01 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -10,83 +10,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title:'Licence',
-      home: FirstScreen()
+      title: 'Licence',
+      home: FirstScreen(),
     );
   }
 }
+
 class FirstScreen extends StatefulWidget {
   const FirstScreen({Key? key}) : super(key: key);
 
   @override
   State<FirstScreen> createState() => _FirstScreenState();
-
 }
-class _FirstScreenState extends State<FirstScreen>{
-   late int age;
-   check() {
-   setState(() {
-   if(age >= 18){
-     showDialog(
-     context: context,
-     builder: (context) {
-     return const AlertDialog(
-     content: Text('Allowed'),
-     );
-     },
-     );
-   debugPrint('allowed');
-   } else{
-     showDialog(
-     context: context,
-     builder: (context) {
-     return const AlertDialog(
-     content: Text('Not Allowed'),
-     );
-     },
-     );
-   debugPrint('not allowed');
-   }
-   });
-      }
 
+class _FirstScreenState extends State<FirstScreen> {
+  late int age;
+  String status = '';
 
+  void check() {
+    if (age >= 18) {
+      status = 'Allowed';
+      debugPrint('allowed');
+    } else {
+      status = 'Not Allowed';
+      debugPrint('not allowed');
+    }
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
-        padding:const EdgeInsets.symmetric(horizontal: 25),
-        child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: SizedBox(
           width: double.infinity,
-          decoration: const BoxDecoration(
-            shape: BoxShape.rectangle,
-          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-               TextField(
-                keyboardType:TextInputType.number,
-                 decoration: const InputDecoration(
-                   labelText: 'Enter your age',
-                   enabledBorder: OutlineInputBorder(
-                     borderSide: BorderSide(width: 3,color: Colors.blue)
-                   ),
-                 ),
-                onChanged: (value){
-                 debugPrint(value);
-                 var val=int.parse(value);
-                 age=val;
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: 'Enter your age',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+                onChanged: (value) {
+                  debugPrint(value);
+                  int parsedAge = int.parse(value);
+                  age = parsedAge;
                 },
               ),
-              TextButton(onPressed: check,
-                  child: const Text('Check'))
+              TextButton(
+                onPressed: check,
+                child: const Text('Check'),
+              ),
+              Text(status)
             ],
           ),
         ),
-      )
+      ),
     );
   }
-
 }
